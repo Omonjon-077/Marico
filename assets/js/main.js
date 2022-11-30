c8c0afc3cf919d4709db39861b03b88d6d092cfe
Omonjon <omonjons077@gmail.com>
2022-11-30 17:36:30 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/
+// const scrollUp = () => {
+//     const scrollUp = document.getElementById('scroll-up')
+//     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+//     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+//         : scrollUp.classList.remove('show-scroll')
+// }
+// window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 2500,
+//     delay: 400,
+//     reset: true
+//   })
+  
+//   sr.reveal('.home__title, .popular__container, .features__img, .featured__filters')
+//   sr.reveal('.home__subtitle', {delay: 500})
+//   sr.reveal('.home__elec', {delay: 600})
+//   sr.reveal('.home__img', {delay: 800})
+//   sr.reveal('.home__car-data', {delay: 900, interval: 100, origin: 'bottom'})
+//   sr.reveal('.home__button', {delay: 1000, origin: 'bottom'})
+//   sr.reveal('.about__group, .offer__data', {origin: 'left'})
+//   sr.reveal('.about__data, .offer__img', {origin: 'right'})
+//   sr.reveal('.features__map', {delay: 600, origin: 'bottom'})
+//   sr.reveal('.features__card', {interval: 300})
+//   sr.reveal('.featured__card, .logos__content, .footer__content', {interval: 100})
